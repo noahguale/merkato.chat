0319012544a116ad5803d4d7bb1b88ddd9385dd0
Noah Guale <noahguale@gmail.com>
2025-06-10 00:59:56 -0500

    Set up the React provider

---
+++ components/sign-in.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useAuthActions } from '@convex-dev/auth/react'
+import { Button } from '@/components/ui/button'
+import { GitHubLogo } from './logos'
+
+export function SignInWithGitHub() {
+	const { signIn } = useAuthActions()
+	return (
+		<Button
+			className="flex-1"
+			variant="outline"
+			type="button"
+			onClick={() => signIn('github')}
+		>
+			<GitHubLogo className="mr-2 h-4 w-4" /> GitHub
+		</Button>
+	)
+}
